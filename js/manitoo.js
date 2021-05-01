94a662d22d6cf337fd6c84cefeb9026bf8d61f8c
s2019w33 <s2019w33@e-mirim.hs.kr>
2021-05-01 15:12:06 +0900

    image number change

--- js/manitoo.js
+++ js/manitoo.js
@@ -1,10 +1,10 @@
 function changeImage() {
     x = parseInt(Math.random() * 18 + 1);
 
-    Imgsrc = `image/${x}.jpg`;
+    Imgsrc = `image/${x+1}.jpg`;
     document.Image.src = Imgsrc;
 
-    text = aboutCh(num);
+    text = aboutCh(x);
     document.getElementById("name").innerText = text;
 }
 function submit() {
@@ -14,7 +14,7 @@ function submit() {
 function aboutCh(num) {
     student_name_list = ['김세이','김아라','김지연','나현아','류지민','맹수연','박서빈','박서은','백민세','손승희','이승민','이승아','이예서','조순형','조현령','조혜령','지유정','최윤경'];
     
-    text = `선택된 학생은 ${student_name_list[num+1]}입니다!`;
+    text = `선택된 학생은 ${student_name_list[num]}입니다!`;
 
     return text;
 }
